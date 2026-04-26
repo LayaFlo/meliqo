c5469e72ee036e862fa333c2315055ff1f861802
nadja <zhur.nadezhda@gmail.com>
2026-04-26 20:58:24 +0300

    add creation provider to add layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { CreationProvider } from "@/src/context/CreationContext";
 import { theme } from "@/src/theme/theme";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
@@ -6,15 +7,17 @@ import { PaperProvider, Surface } from "react-native-paper";
 export default function RootLayout() {
   return (
     <PaperProvider theme={theme}>
-      <Surface style={{ flex: 1 }}>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        />
-      </Surface>
+      <CreationProvider>
+        <Surface style={{ flex: 1 }}>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          />
+        </Surface>
 
-      <StatusBar style="light" />
+        <StatusBar style="light" />
+      </CreationProvider>
     </PaperProvider>
   );
 }
